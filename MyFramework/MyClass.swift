e18517f8f59d0820c9c1ac45598165eb7e331715
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2022-07-07 15:32:24 +0700

    Create Modular Arc

---
+++ MyFramework/MyClass.swift
@@ -0,0 +1,19 @@
+//
+//  MyClass.swift
+//  MyFramework
+//
+//  Created by Timotius Leonardo Lianoto on 07/07/22.
+//
+
+import Foundation
+
+public class MyClass {
+    
+    public init() {}
+    
+    public let foo = "foo"
+}
+
+class MyClassInternal {
+    let bar = "bar"
+}
